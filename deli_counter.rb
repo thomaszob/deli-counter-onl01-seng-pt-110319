85cfbfe2f65ca31d34d91a9f2b9e100a15df8bf4
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-15 02:17:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,9 @@ def take_a_number(array, name)
   array.push(name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end 
+
+def now_serving(array)
+  if array.empty? 
+    puts "The line is empty."
+  end 
+end 

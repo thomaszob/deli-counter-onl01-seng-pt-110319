80c57fc98b39b36ec2f082c9b22789432d32794c
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-11-15 02:11:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,11 @@
 # Write your code here.
 def line(array)
+  nameList = ""
   if array.empty?
   puts "The line is currently empty."
 else
   array.each_with_index{|name, index|
-    nameList  " #{index + 1}. #{name}"
+    nameList  << " #{index + 1}. #{name}"
   }
   end
   puts "The line is currently #{nameList}"
